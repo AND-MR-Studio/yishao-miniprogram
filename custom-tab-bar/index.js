f681b2b5ae04561bc2ac1cfec43216b82cf0abb3
yavin666 <5475173+yavin666@user.noreply.gitee.com>
2025-04-01 22:55:15 +0800

    feat(app): 更新底部导航栏配置并添加页面显示逻辑

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,56 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    selected: 1,
+    color: "#DBFFEC",
+    selectedColor: "#5CE821",
+    list: [
+      {
+        pagePath: "/pages/index/index",
+        text: "论坛",
+        icon: "icon-community_UnSelected",
+        selectedIcon: "icon-community_Selected"
+      },
+      {
+        pagePath: "/pages/dialog/dialog",
+        text: "喝汤",
+        icon: "icon-soup_UnSelected",
+        selectedIcon: "icon-soup_Selected"
+      },
+      {
+        pagePath: "/pages/mine/mine",
+        text: "我的",
+        icon: "icon-my_UnSelected",
+        selectedIcon: "icon-my_Selected"
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset;
+      const url = data.path;
+      
+      wx.switchTab({
+        url,
+        success: () => {
+          this.setData({
+            selected: data.index
+          });
+        }
+      });
+    }
+  }
+}) 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,10 @@
 // index.js
-Page({})
+Page({
+  onShow() {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0
+      });
+    }
+  }
+})
