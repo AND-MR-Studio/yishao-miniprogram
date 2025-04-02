734198fd60f01dc1ed36db85aa7a3ee604b93a35
yavin666 <5475173+yavin666@user.noreply.gitee.com>
2025-04-02 13:25:12 +0800

    feat(community, dialog, mine): 更新对话和个人中心页面结构

--- pages/community/community.js
+++ pages/community/community.js
@@ -1,6 +1,34 @@
 // index.js
 Page({
-  onShow() {
-
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 页面数据
   },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0  // 修改为1，因为社区页面是第二个tab
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    // 页面加载时的逻辑
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    // 页面初次渲染完成时的逻辑
+  }
 })
