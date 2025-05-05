f96dbd7aed4fb594514bbbbde2ab0c16bd98a87f
yavin666 <5475173+yavin666@user.noreply.gitee.com>
2025-05-05 20:19:22 +0800

    feat: Implement main page for Soup Game with navigation, display components, and state management

--- utils/config.js
+++ utils/config.js
@@ -5,20 +5,20 @@ const config = {
   //开发版
   'develop': {
     //请求的地址
-    baseUrl: 'https://and-tech.cn',
-    ysUrl: 'https://and-tech.cn/yishao-api/'
+    baseUrl: 'https://yavin.and-tech.cn',
+    ysUrl: 'https://yavin.and-tech.cn/yishao-api/'
   },
   //体验版
   'trial': {
     //请求的地址
-    baseUrl: 'https://and-tech.cn',
-    ysUrl: 'https://and-tech.cn/yishao-api/'
+    baseUrl: 'https://yavin.and-tech.cn',
+    ysUrl: 'https://yavin.and-tech.cn/yishao-api/'
   },
   //正式版
   'release': {
     //请求的地址
-    baseUrl: 'https://and-tech.cn',
-    ysUrl: 'https://and-tech.cn/yishao-api/'
+    baseUrl: 'https://yavin.and-tech.cn',
+    ysUrl: 'https://yavin.and-tech.cn/yishao-api/'
   }
 }[wx.getAccountInfoSync().miniProgram.envVersion];
 
