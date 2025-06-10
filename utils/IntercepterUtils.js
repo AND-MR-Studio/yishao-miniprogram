10258255138a03c4fb2792f5cd3e4025fb8e1f3e
yavin666 <30970700+yavin666@users.noreply.github.com>
2025-06-10 15:21:21 +0800

    Update IntercepterUtils.js

--- utils/IntercepterUtils.js
+++ utils/IntercepterUtils.js
@@ -1,13 +1,10 @@
 const {addRequestInterceptor, addResponseInterceptor} = require('request');
-
+const {userStore} = require('../stores/index');
 /**
  * 业务拦截器工具
  */
 
 addRequestInterceptor((config) => {
-    // 延迟加载 userStore，避免循环依赖
-    const {userStore} = require('../stores/index');
-    
     let userId = userStore.userId;
     config.header = config.header || {};
     config.header['X-User-Id'] = userId ? userId : 'guest';
